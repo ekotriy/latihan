3f9f291846cbe77a2fd58852191a4390cb7cbbf9
ekotriy <ekotriy98@gmail.com>
2023-01-26 10:59:20 +0700

    searching & pagination - menambahkan query searching

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.main')
 @section('container')
-<h1 class="mb-5">{{ $title }}</h1>
+<h1 class="mb-3 text-center">{{ $title }}</h1>
+
+<div class="row justify-content-center">
+  <div class="col-md-6">
+    <form action="/posts">
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Search..." name="search" value="{{ request('search') }}">
+        <button class="btn btn-danger" type="submit">Search</button>
+      </div>
+    </form>
+  </div>
+</div>
 
 @if($posts->count())
 <div class="card mb-3">
