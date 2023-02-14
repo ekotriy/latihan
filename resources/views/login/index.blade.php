964770eb611b803744b99e2982b1da1a273780f8
ekotriy <ekotriy98@gmail.com>
2023-02-14 15:26:14 +0700

    user registration

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.main')
 @section('container')
     <div class="row justify-content-center">
-        <div class="col-lg-5">
+        <div class="col-md-4">
+            @if (session()->has('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{session('success')}}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            @endif
             <main class="form-signin w-100 m-auto">
                 <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
                 <form>
@@ -15,7 +21,7 @@
                     </div>
                     <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
                 </form>
-                <small class="d-block text-center mt-3">Not registered? <a href="/register">Register Now!</a></small>
+                <small class="d-block text-center mt-3">Not registered? <a class="text-decoration-none" href="/register">Register Now!</a></small>
             </main>
         </div>
     </div>
